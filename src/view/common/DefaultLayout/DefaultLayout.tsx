b343655b3f3fddd9729741833afeafff80baf939
Shachiru <shachirurashmika35@gmail.com>
2025-07-11 22:02:31 +0530

    footer modified

--- src/view/common/DefaultLayout/DefaultLayout.tsx
+++ src/view/common/DefaultLayout/DefaultLayout.tsx
@@ -1,6 +1,6 @@
-import {Footer} from "../Footer/Footer.tsx";
 import {MainContent} from "../MainContent/MainContent.tsx";
 import {NavBar} from "../NavBar/NavBar.tsx";
+import Footer from "../Footer/Footer.tsx";
 
 export function DefaultLayout() {
     return (
